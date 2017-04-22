df92ae679d709dfe504893ae1d9833e8e0ead1a2
Daniel Tabak <daniel@tabak.ca>
2017-04-22 19:02:59 -0400

    lol

--- server.js
+++ server.js
@@ -8,19 +8,11 @@ let app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/', (req, res) => {
-<<<<<<< HEAD
   let resp = new twilio.TwimlResponse();
   resp.play({digits: [request.query.digit || 6]});
   res.header('Content-Type', 'text/xml');
   res.send(resp.toString());
-=======
-  requestAccess().then(() => {
-    let resp = new twilio.TwimlResponse();
-    resp.play({digits: [6]});
-    res.header('Content-Type', 'text/xml');
-    res.send(resp.toString());
-  });
->>>>>>> 46b23995990d21c1173fc252b655e3a9bf6a5d4d
+
 });
 
 app.listen((process.env.PORT || 5000), function () {
