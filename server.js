cece66ed4ee10a8581336017ed49cec99caa0176
Daniel Tabak <daniel@tabak.ca>
2017-04-22 20:26:13 -0400

    lol

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/', (req, res) => {
   let resp = new twilio.TwimlResponse();
-  resp.play({digits: [req.query.digit || 6]});
+  resp.play({digits: [req.query.digit] || [9, 8, 6, 1, 2, 3, 4, 5, 7, 0]});
   res.header('Content-Type', 'text/xml');
   res.send(resp.toString());
 
